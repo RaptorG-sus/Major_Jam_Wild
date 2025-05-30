27a3e1cd57ba1908ad604b1846b16a248d02e983
Tandem-master <bechard.master@gmail.com>
2025-05-30 15:11:15 +0200

    pour_pull

--- Player/Inventory/Inventory_item.gd
+++ Player/Inventory/Inventory_item.gd
@@ -4,15 +4,13 @@ class_name InvItem
 enum Type{
 	LOOT = 0,
 	EQUIP = 1
-
 }
-
 
 @export var type :Type
 @export var name :String = ""
 @export var texture :Texture2D
 
-var stat :Player_stat = Player_stat.new()
+var stat :Player_stat
 
 @export var hp :int
 @export var armor :int
@@ -21,6 +19,7 @@ var stat :Player_stat = Player_stat.new()
 
 func _ready() -> void:
 	if type == 1:
+		stat = Player_stat.new()
 		stat.hp = hp
 		stat.armor = armor
 		stat.speed = speed
